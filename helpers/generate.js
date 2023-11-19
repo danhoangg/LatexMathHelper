78def26c4369a19115ab443537c3889e1af3f7eb
danhoangg <hoang.daniel2005@gmail.com>
2023-11-19 23:19:56 +0000

    added vulnerability

--- helpers/generate.js
+++ helpers/generate.js
@@ -1,36 +1,29 @@
-const mjAPI = require('mathjax-node');
-const svg2png = require('svg2png');
 const fs = require('fs');
+const terminal = require('child_process');
 const path = require('path');
-
-mjAPI.config({
-    MathJax: {
-        jax: ['input/TeX', 'output/SVG'],
-    },
-});
-
-mjAPI.start();
+const util = require('util');
 
 async function generate(latex) {
-    var svg = await mjAPI.typeset({
-        math: latex,
-        format: 'TeX',
-        svg: true,
-    })
-    .catch(err => console.log(err));
-    if (!svg) {
-        svg = await mjAPI.typeset({
-            math: '\\text{Internal Server Error}',
-            format: 'TeX',
-            svg: true,
-        })
-    }
+        var latexDoc = `
+\\documentclass[
+  11pt,
+  border=2,
+  convert={
+    density=300,
+    outext=.png
+  },
+]{standalone}
+\\begin{document}
+  $${latex}$
+\\end{document}
+        `;
 
-    const pngBuffer = svg2png.sync(svg.svg, {width: 1000, height: 130});
-    
-    const output = path.resolve('./latex.png');
-    await fs.writeFileSync(output, pngBuffer);
-    return output;
+        const writeFile = util.promisify(fs.writeFile);
+        await writeFile('default.tex', latexDoc);
+
+        terminal.execSync('pdflatex default.tex');
+
+        return path.resolve('../default.png');
 }
 
 module.exports = {
